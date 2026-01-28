b11a8343b0d5189bf19c65340afc1376fa7a51c4
neksen <s.kravtsov@hotmail.com>
2026-01-28 22:44:20 +0200

    add App "Wallets"

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'users',
     'services',
     'appointments',
+    'wallets',
 ]
 
 MIDDLEWARE = [
---
+++ wallets/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from wallets.models import ClientWallet, ClientWalletTransaction
+
+
+# Register your models here.
+@admin.register(ClientWallet)
+class ClientWalletAdmin(admin.ModelAdmin):
+    list_display = ('client', 'balance', 'created_at', 'updated_at')
+    list_filter = ('client',)
+
+
+@admin.register(ClientWalletTransaction)
+class ClientWalletTransactionAdmin(admin.ModelAdmin):
+    list_display = ('wallet', 'amount', 'type', 'service_record', 'created_at')
+    list_filter = ('type',)
