acc212a60d45925748d3dfbbe89352cd23a52a57
s.kravtsov <s.kravtsov@hotmail.com>
2026-02-10 14:54:22 +0200

    add index on service field "is_active"

--- services/models.py
+++ services/models.py
@@ -6,7 +6,7 @@ class Service(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     base_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
