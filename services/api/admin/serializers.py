9896afdeb50988feabdd3f69f89d743ae76f8638
s.kravtsov <s.kravtsov@hotmail.com>
2026-02-10 17:30:16 +0200

    fix validate in AdminSpecialistServiceSerializer

--- services/api/admin/serializers.py
+++ services/api/admin/serializers.py
@@ -32,24 +32,24 @@ class AdminSpecialistServiceSerializer(serializers.ModelSerializer):
         )
         read_only_fields = ("id", "created_at", "updated_at")
 
-        def validate(self, attrs):
-            service = attrs["service"]
-            payout_type = attrs["payout_type"]
-            payout_value = attrs["payout_value"]
+    def validate(self, attrs):
+        service = attrs["service"]
+        payout_type = attrs["payout_type"]
+        payout_value = attrs["payout_value"]
 
-            if service and not service.is_active:
-                raise serializers.ValidationError(
-                    {"service": "Cannot assign archived service."}
-                )
+        if service and not service.is_active:
+            raise serializers.ValidationError(
+                {"service": "Cannot assign archived service."}
+            )
 
-            if payout_type == SpecialistService.Type.FULL and payout_value:
-                raise serializers.ValidationError(
-                    {"payout_value": "Should be empty when payout type is FULL."}
-                )
+        if payout_type == SpecialistService.Type.FULL and payout_value:
+            raise serializers.ValidationError(
+                {"payout_value": "Should be empty when payout type is FULL."}
+            )
 
-            if payout_type == SpecialistService.Type.FIXED and not payout_value:
-                raise serializers.ValidationError(
-                    {"payout_value": "Required when payout type is FIXED."}
-                )
+        if payout_type == SpecialistService.Type.FIXED and not payout_value:
+            raise serializers.ValidationError(
+                {"payout_value": "Required when payout type is FIXED."}
+            )
 
-            return attrs
+        return attrs
