ca5b4819e65c02f92ae46e518a89b43da7ae1a15
s.kravtsov <s.kravtsov@hotmail.com>
2026-02-10 14:54:44 +0200

    fix slug for public API services

--- services/api/public/urls.py
+++ services/api/public/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from services.api.public.views import ServicesViewSet
 
 router = DefaultRouter()
-router.register("services", ServicesViewSet, basename="services")
+router.register("services", ServicesViewSet, basename="public-services")
 
 urlpatterns = router.urls
